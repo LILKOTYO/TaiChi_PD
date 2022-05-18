59307a8f7f61771c3c868a80f6bab2a00f51fe3b
LILKOTYO <lil-kotyo@sjtu.edu.cn>
2022-05-18 23:18:43 +0800

    feat: (2022/05/18) change something

--- cloth.py
+++ cloth.py
@@ -1,5 +1,5 @@
 import taichi as ti
-ti.init(arch=ti.cuda) # Alternatively, ti.init(arch=ti.cpu)
+ti.init(arch=ti.cuda)
 
 N = 128
 cell_size = 1.0 / N
@@ -8,23 +8,26 @@ stiffness = 1600
 damping = 2
 dt = 5e-4
 
+# define the ball
 ball_radius = 0.2
-ball_center = ti.Vector.field(3, float, (1,))
+ball_center = ti.Vector.field(3, ti.f32, (1,))
 
-x = ti.Vector.field(3, float, (N, N))
-v = ti.Vector.field(3, float, (N, N))
+# use ti.f32 instead of float, will be different?
+# the N x N array makes my particles (1-D array) a joke 0.0
+x = ti.Vector.field(3, ti.f32, (N, N))
+v = ti.Vector.field(3, ti.f32, (N, N))
 
 num_triangles = (N - 1) * (N - 1) * 2
 indices = ti.field(int, num_triangles * 3)
-vertices = ti.Vector.field(3, float, N * N)
+vertices = ti.Vector.field(3, ti.f32, N * N)
 
 
 def init_scene():
     for i, j in ti.ndrange(N, N):
-        x[i, j] = ti.Vector([i * cell_size ,
+        x[i, j] = ti.Vector([i * cell_size,
                              j * cell_size / ti.sqrt(2),
                              (N - j) * cell_size / ti.sqrt(2)])
-    ball_center[0] = ti.Vector([0.5, -0.5, -0.0])
+        ball_center[0] = ti.Vector([0.5, -0.5, -0.0])
 
 
 @ti.kernel
@@ -42,7 +45,8 @@ def set_indices():
             indices[square_id * 6 + 5] = (i + 1) * N + j
 
 
-links = [[-1, 0], [1, 0], [0, -1], [0, 1], [-1, -1], [1, -1], [-1, 1], [1, 1]]
+# init the links (constraints / edges)
+links = [[-1, 0], [1, 0], [0, -1], [0, 1], [1, 1], [1, -1], [-1, 1], [-1, -1]]
 links = [ti.Vector(v) for v in links]
 
 
@@ -51,9 +55,10 @@ def step():
     for i in ti.grouped(x):
         v[i].y -= gravity * dt
     for i in ti.grouped(x):
-        force = ti.Vector([0.0,0.0,0.0])
+        force = ti.Vector([0.0, 0.0, 0.0])
         for d in ti.static(links):
-            j = min(max(i + d, 0), [N-1,N-1])
+            # clamp the coordinate into the range of 0x0 ~ N-1xN-1
+            j = min(max(i + d, 0), [N-1, N-1])
             relative_pos = x[j] - x[i]
             current_length = relative_pos.norm()
             original_length = cell_size * float(i-j).norm()
@@ -62,7 +67,7 @@ def step():
         v[i] += force * dt
     for i in ti.grouped(x):
         v[i] *= ti.exp(-damping * dt)
-        if (x[i]-ball_center[0]).norm() <= ball_radius:
+        if (x[i] - ball_center[0]).norm() <= ball_radius:
             v[i] = ti.Vector([0.0, 0.0, 0.0])
         x[i] += dt * v[i]
 
@@ -76,7 +81,7 @@ def set_vertices():
 init_scene()
 set_indices()
 
-window = ti.ui.Window("Cloth", (800, 800), vsync=True)
+window = ti.ui.Window("Cloth Simulation", (800, 800), vsync=True)
 canvas = window.get_canvas()
 scene = ti.ui.Scene()
 camera = ti.ui.make_camera()
@@ -91,7 +96,7 @@ while window.running:
     scene.set_camera(camera)
 
     scene.point_light(pos=(0.5, 1, 2), color=(1, 1, 1))
-    scene.mesh(vertices, indices=indices, color=(0.5, 0.5, 0.5), two_sided = True)
+    scene.mesh(vertices, indices=indices, color=(0.2, 0.2, 0.3), two_sided=True)
     scene.particles(ball_center, radius=ball_radius, color=(0.5, 0, 0))
     canvas.scene(scene)
     window.show()
--- pd.py
+++ pd.py
@@ -1,43 +1,37 @@
 import taichi as ti
+import math
+import numpy as np
+import cv2
+import os
+import matplotlib.pyplot as plt
 
-ti.init(arch=ti.gpu)
+real = ti.f32
+ti.init(arch=ti.gpu, default_fp=real)
 
 # try to simulate a 64x64x64 soft body
 dim_length = 64
 dim_width = 64
 dim_height = 64
 
-max_num_particles = dim_length * dim_width * dim_height
-particles = ti.Vector.field(3, ti.f64, shape=max_num_particles)
+max_steps = 1024
+n_objects = dim_length * dim_width * dim_height
+scalar = lambda: ti.field(dtype=real)
+vec = lambda: ti.Vector.field(2, dtype=real)
 
+x = vec()
+v = vec()
+force = vec()
 
-def particleAt(x, y, z):
-    idx = z * dim_length * dim_width + y * dim_length + x
-    return particles[idx]
-
-
-def particleIdx(x, y, z):
-    idx = z * dim_length * dim_width + y * dim_length + x
-    return idx
-
-
-@ti.func
-def particleCoor(idx):
-    tmp = idx
-    z = int(tmp / (dim_length * dim_width))
-    tmp = tmp % (dim_length * dim_width)
-    y = int(tmp / dim_length)
-    x = tmp % dim_length
-    return x, y, z
+# is too big to store all ltime step information!
+def allocate_fields():
+    ti.root.dense(ti.i, max_steps).dense(ti.j, max_steps).place(x, v, force)
 
 
 @ti.kernel
-def init():
-    # init position
-    for idx in range(dim_length * dim_width * dim_height):
-        x, y, z = particleCoor(idx)
-        particles[idx][0] = x
-        particles[idx][1] = y
-        particles[idx][2] = z
+def reset():
+    for I in ti.grouped(x):
+        print(x[I])
 
 
+allocate_fields()
+reset()
