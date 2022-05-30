5e4c0c0153f5ef23bc7b686ef9010c1480e071fe
LILKOTYO <lil-kotyo@sjtu.edu.cn>
2022-05-30 20:20:18 +0800

    feat: (2022/05/30) initialize setting

--- pd.py
+++ pd.py
@@ -1,133 +1,227 @@
-# this is a pd implementation in a games 103 style.
-# the difference between the pd paper is that the gradient of energy W is more physical
-
 import taichi as ti
 import math
-import numpy as np
-import cv2
-import os
-import matplotlib.pyplot as plt
 
-real = ti.f32
-ti.init(arch=ti.gpu, default_fp=real)
+ti.init(arch=ti.cpu)
 
-# try to simulate a 64x64x64 soft body
-# length: x     width: y    height: z
-dim_length = 9
-dim_width = 9
-dim_height = 9
-cell_size = 1.0 / dim_height
+# procedurally setting up the cantilever
+init_x, init_y, init_z = 0.4, 0.4, 0.4
+N_x = 10
+N_y = 10
+N_z = 10
+# N_x = 2
+# N_y = 2
+N = N_x * N_y * N_z
+# axis-x + axis-y + axis-z + diagonal_xy + diagonal_xz + diagonal_yz
+N_edges = (N_x - 1) * N_y * N_z + (N_y - 1) * N_x * N_z + (N_z - 1) * N_x * N_y \
+    + (N_x - 1) * (N_y - 1) * N_z + (N_x - 1) * (N_z - 1) * N_y + (N_y - 1) * (N_z - 1) * N_x
+N_tetrahedron = 5 * (N_x - 1) * (N_y - 1) * (N_z - 1)
+dx = 0.4/N_x
 
-mass = 1.0
-stiffness = 50
-max_steps = 1024
-n_objects = dim_length * dim_width * dim_height
-scalar = lambda: ti.field(dtype=real)
-vec = lambda: ti.Vector.field(3, dtype=real)
+# physical quantities
+m = 1
+g = 9.8
+YoungsModulus = ti.field(ti.f32, ())
+PoissonsRatio = ti.field(ti.f32, ())
+LameMu = ti.field(ti.f32, ())
+LameLa = ti.field(ti.f32, ())
 
-# vectors init
-q = scalar()
-last_q = scalar()
-p = scalar()
-v = scalar()
-force = scalar()
-vertices = ti.Vector.field(3, real, n_objects)
-link_anchor_a = ti.field(ti.i32)
-link_anchor_b = ti.field(ti.i32)
-link_length = scalar()
+# time-step size (for simulation, 16.7ms)
+h = 16.7e-3
+# substepping
+substepping = 100
+# time-step size (for time integration)
+dh = h/substepping
 
-# matrix init
-M_builder = ti.linalg.SparseMatrixBuilder(3*n_objects, 3*n_objects, max_num_triplets=3*n_objects)
-H_builder = ti.linalg.SparseMatrixBuilder(3*n_objects, 3*n_objects, max_num_triplets=3*n_objects)
+# simulation components
+x = ti.Vector.field(3, ti.f32, N)
+v = ti.Vector.field(3, ti.f32, N)
+total_energy = ti.field(ti.f32, ())
+grad = ti.Vector.field(3, ti.f32, N)
+elements_Dm_inv = ti.Matrix.field(3, 3, ti.f32, N_tetrahedron)
+elements_V0 = ti.field(ti.f32, N_tetrahedron)
 
-# init the links (constraints / edges) and the origin length
-links = [[1, 0, 0], [1, 1, 0], [1, -1, 0], [1, 0, 1], [1, 0, -1],
-         [-1, 0, 0], [-1, 1, 0], [-1, -1, 0], [-1, 0, 1], [-1, 0, -1],
-         [0, 1, 0], [0, -1, 0], [0, 0, 1], [0, 0, -1],
-         [0, 1, 1], [0, 1, -1], [0, -1, 1], [0, -1, -1]]
-sqrt2 = ti.sqrt(2)
-len = [1, sqrt2, sqrt2, sqrt2, sqrt2, 1, sqrt2, sqrt2, sqrt2, sqrt2, 1, 1, 1, 1, sqrt2, sqrt2, sqrt2, sqrt2]
-links = [ti.Vector(v) for v in links]
+# geometric components
+tetrahedrons = ti.Vector.field(4, ti.i32, N_tetrahedron)
+edges = ti.Vector.field(2, ti.i32, N_edges)
 
 
 @ti.func
-def copy(src: ti.template(), dst: ti.template()):
-    assert src.shape == dst.shape
-    for I in ti.grouped(src):
-        dst[I] = src[I]
+def ijk_2_index(i, j, k): return k * N_x * N_y + i * N_y + j
 
 
-def allocate_fields():
-    ti.root.dense(ti.i, 3*n_objects).place(p, q, last_q, v, force)
+# -----------------------meshing and init----------------------------
+@ti.kernel
+def meshing():
+    # setting up tetrahedrons
+    for i, j, k in ti.ndrange(N_x - 1, N_y - 1, N_z - 1):
+        # tetrahedron id
+        tid = k * (N_x - 1) * (N_y - 1) + i * (N_y - 1) + j
+        tetrahedrons[tid][0] = ijk_2_index(i, j, k)
+        tetrahedrons[tid][1] = ijk_2_index(i + 1, j, k)
+        tetrahedrons[tid][2] = ijk_2_index(i + 1, j + 1, k)
+        tetrahedrons[tid][3] = ijk_2_index(i + 1, j, k + 1)
 
+        tid = k * (N_x - 1) * (N_y - 1) + i * (N_y - 1) + j + 1
+        tetrahedrons[tid][0] = ijk_2_index(i, j + 1)
+        tetrahedrons[tid][1] = ijk_2_index(i + 1, j + 1)
+        tetrahedrons[tid][2] = ijk_2_index(i + 1, j)
+        tetrahedrons[tid][3] =
 
+    # setting up edges
+    # edge id
+    eid_base = 0
+
+    # horizontal edges
+    for i in range(N_x-1):
+        for j in range(N_y):
+            eid = eid_base+i*N_y+j
+            edges[eid] = [ij_2_index(i, j), ij_2_index(i+1, j)]
+
+    eid_base += (N_x-1)*N_y
+    # vertical edges
+    for i in range(N_x):
+        for j in range(N_y-1):
+            eid = eid_base+i*(N_y-1)+j
+            edges[eid] = [ij_2_index(i, j), ij_2_index(i, j+1)]
+
+    eid_base += N_x*(N_y-1)
+    # diagonal edges
+    for i in range(N_x-1):
+        for j in range(N_y-1):
+            eid = eid_base+i*(N_y-1)+j
+            edges[eid] = [ij_2_index(i+1, j), ij_2_index(i, j+1)]
+
+@ti.kernel
+def initialize():
+    YoungsModulus[None] = 1e6
+    paused = True
+    # init position and velocity
+    for i, j in ti.ndrange(N_x, N_y):
+        index = ij_2_index(i, j)
+        x[index] = ti.Vector([init_x + i * dx, init_y + j * dx])
+        v[index] = ti.Vector([0.0, 0.0])
+
+# both Ds and Dm?         F = DsDm^-1
 @ti.func
-def get_coordinate(idx: ti.i32):
-    z = idx // (dim_length * dim_width)
-    tmp1 = idx % (dim_length * dim_width)
-    y = tmp1 // dim_length
-    x = tmp1 % dim_length
-    return x, y, z
+def compute_D(i):
+    a = triangles[i][0]
+    b = triangles[i][1]
+    c = triangles[i][2]
+    return ti.Matrix.cols([x[b] - x[a], x[c] - x[a]])
 
+@ti.kernel
+def initialize_elements():
+    for i in range(N_triangles):
+        Dm = compute_D(i)
+        elements_Dm_inv[i] = Dm.inverse()
+        elements_V0[i] = ti.abs(Dm.determinant())/2
 
+# ----------------------core-----------------------------
 @ti.func
-def get_index(x: ti.i32, y: ti.i32, z: ti.i32):
-    return x + y * dim_length + z * dim_length * dim_width
+def compute_R_2D(F):
+    R, S = ti.polar_decompose(F, ti.f32)
+    return R
+
+@ti.kernel
+def compute_gradient():
+    # clear gradient
+    for i in grad:
+        grad[i] = ti.Vector([0, 0])
+
+    # gradient of elastic potential
+    for i in range(N_triangles):
+        Ds = compute_D(i)
+        # elements_Dm_inv is initialized in the function named initialize_elements()
+        F = Ds@elements_Dm_inv[i]
+        # co-rotated linear elasticity
+        R = compute_R_2D(F)
+        Eye = ti.Matrix.cols([[1.0, 0.0], [0.0, 1.0]])
+        # first Piola-Kirchhoff tensor
+        P = 2*LameMu[None]*(F-R) + LameLa[None]*((R.transpose())@F-Eye).trace()*R
+        # assemble to gradient
+        # H is the derivative of energy to the position
+        H = elements_V0[i] * P @ (elements_Dm_inv[i].transpose())
+        a,b,c = triangles[i][0],triangles[i][1],triangles[i][2]
+        gb = ti.Vector([H[0,0], H[1, 0]])
+        gc = ti.Vector([H[0,1], H[1, 1]])
+        ga = -gb-gc
+        grad[a] += ga
+        grad[b] += gb
+        grad[c] += gc
+
+@ti.kernel
+def compute_total_energy():
+    for i in range(N_triangles):
+        Ds = compute_D(i)
+        F = Ds @ elements_Dm_inv[i]
+        # co-rotated linear elasticity
+        R = compute_R_2D(F)
+        Eye = ti.Matrix.cols([[1.0, 0.0], [0.0, 1.0]])
+        element_energy_density = LameMu[None]*((F-R)@(F-R).transpose()).trace() + 0.5*LameLa[None]*(R.transpose()@F-Eye).trace()**2
+
+        total_energy[None] += element_energy_density * elements_V0[i]
+
+@ti.kernel
+def update():
+    # perform time integration
+    for i in range(N):
+        # symplectic integration
+        # elastic force + gravitation force, dividing mass to get the acceleration
+        acc = -grad[i] / m - ti.Vector([0.0, g])
+        v[i] += dh * acc
+
+        x[i] += dh*v[i]
+
+    # explicit damping (ether drag)
+    for i in v:
+        v[i] *= ti.exp(-dh*5)
+
+    # enforce boundary condition
+    for j in range(N_y):
+        ind = ij_2_index(0, j)
+        v[ind] = ti.Vector([0, 0])
+        x[ind] = ti.Vector([init_x, init_y + j * dx])  # rest pose attached to the wall
+
+    for i in range(N):
+        if x[i][0] < init_x:
+            x[i][0] = init_x
+            v[i][0] = 0
 
 
 @ti.kernel
-def reset():
-    for i in range(n_objects):
-        x, y, z = get_coordinate(i)
-        x[i] = ti.Vector([x, y, z])
-        v[i] = ti.Vector([0.0, 0.0, 0.0])
-        force[i] = ti.Vector([0.0, 0.0, 0.0])
+def updateLameCoeff():
+    E = YoungsModulus[None]
+    nu = PoissonsRatio[None]
+    LameLa[None] = E*nu / ((1+nu)*(1-2*nu))
+    LameMu[None] = E / (2*(1+nu))
 
+# init once and for all
+meshing()
+initialize()
+initialize_elements()
+updateLameCoeff()
 
-@ti.func
-def update_force():
-    for a in range(n_objects):
-        a_x, a_y, a_z = get_coordinate(a)
-        for k in ti.grouped(links):
-            b_x = a_x + links[k][0]
-            b_y = a_y + links[k][1]
-            b_z = a_z + links[k][2]
-            b = get_index(b_x, b_y, b_z)
-            ab = p[b] - p[a]
-            current_len = ab.norm()
-            force[a] += stiffness * ab.normalized() * (current_len - len[k])
+gui = ti.GUI('Linear FEM', (800, 800))
+while gui.running:
+    # numerical time integration
+    for i in range(substepping):
+        compute_gradient()
+        update()
 
+    # render
+    pos = x.to_numpy()
+    for i in range(N_edges):
+        a, b = edges[i][0], edges[i][1]
+        gui.line((pos[a][0], pos[a][1]),
+                 (pos[b][0], pos[b][1]),
+                 radius=1,
+                 color=0xFFFF00)
+    gui.line((init_x, 0.0), (init_x, 1.0), color=0xFFFFFF, radius=4)
 
-@ti.func
-def pre_calculate():
-    # Set the mass matrix and the Hessian matrix
-    dim = 3 * n_objects
-    for i in range(dim):
-        M_builder[i, i] += mass
-    for
+    # text
+    gui.text(
+        content=f'9/0: (-/+) Young\'s Modulus {YoungsModulus[None]:.1f}', pos=(0.6, 0.9), color=0xFFFFFF)
+    gui.text(
+        content=f'7/8: (-/+) Poisson\'s Ratio {PoissonsRatio[None]:.3f}', pos=(0.6, 0.875), color=0xFFFFFF)
 
-@ti.kernel
-def forward():
-    pre_calculate()
-    for i in range(max_steps):
-
-
-
-@ti.kernel
-def clear_tensor():
-    for i in range(n_objects):
-        v[i] = ti.Vector([0.0, 0.0, 0.0])
-        force[i] = ti.Vector([0.0, 0.0, 0.0])
-
-
-@ti.kernel
-def set_vertices():
-    for i in range(n_objects):
-        vertices[i] = p[i]
-
-
-allocate_fields()
-reset()
-
-
-
+    gui.show()
