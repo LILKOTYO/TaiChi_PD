99759bc39fcdeac9c554ac98c642df722573ecc8
LILKOTYO <lil-kotyo@sjtu.edu.cn>
2022-06-03 15:55:58 +0800

    feat: (2022/06/03) finish all the init settings

--- implicitFEM_3D.py
+++ implicitFEM_3D.py
@@ -34,23 +34,40 @@ dh = h / N_substeps
 scalar = lambda: ti.field(dtype=ti.f32)
 vec = lambda: ti.Vector.field(3, dtype=ti.f32)
 mac3x3 = lambda: ti.Matrix.field(3, 3, dytpe=ti.f32)
-mac12x12 = lambda: ti.Matrix.field(12, 12, dtype=ti.f32)
+
 # vectors and matrixs
 x = ti.Vector.field(3, ti.f32, N)
 v = ti.Vector.field(3, ti.f32, N)
 elements_Dm_inv = ti.Matrix.field(3, 3, ti.f32, N_tetrahedron)
-elements_V0 = ti.field(ti.f32, N_tetrahedron)
+# elements_V0 = ti.field(ti.f32, N_tetrahedron)
+elements_V0 = ti.field(ti.f32, 5)
 f = ti.Vector.field(3, ti.f32, N)
-# tensors
-delta_f = vec()
-K = mac12x12()
 
 # geometric components
 tetrahedrons = ti.Vector.field(4, ti.i32, N_tetrahedron)
 edges = ti.Vector.field(2, ti.i32, N_edges)
 
+# derivatives
+dD = mac3x3()
+dF = mac3x3()
+dP = mac3x3()
+dH = mac3x3()
+
 # allocate_tensors
-# delta_f[i][j][k]: in the i-th tetrahedron,
-# the delta force acting on the k-th mass point, which caused by
-# the j-th component in the position vector
-ti.root.dense(ti.i, N_tetrahedron).dense(ti.i, 12).dense(ti.j, 4).place(delta_f)
+# dD[i][j]: the difference caused by the change of j-th component in the i-th node
+ti.root.dense(ti.i, 4).dense(ti.j, 3).place(dD)
+# dD[k][i][j]: the difference of the k-th tetrahedron's F in a cube
+# caused by the change of j-th component in the i-th node
+ti.root.dense(ti.i, 5).dense(ti.i, 4).dense(ti.j, 3).place(dF)
+# dP[e][i][j]: the difference of the e-th tetrahedron's P
+# cause by the change of the j-th component in the i-th node
+ti.root.dense(ti.i, N_tetrahedron).dense(ti.i, 4).dense(ti.j, 3).place(dP, dH)
+
+# df/dx
+K_builder = ti.linalg.SparseMatrixBuilder(3*N, 3*N, max_num_triplets=9*N*N)
+
+# for solving system of linear equations
+A_builder = ti.linalg.SparseMatrixBuilder(3*N, 3*N, max_num_triplets=9*N*N)
+b = ti.field(ti.f32, shape=3*N)
+x = ti.field(ti.f32, shape=3*N)
+
